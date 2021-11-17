67d0333e7dae1b58f98ba90b8b526ac372105091
unknown <bennyizkandar@gmail.com>
2021-11-17 18:23:56 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+try {
+  require("electron-reloader")(module);
+} catch (_) {}
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  win.loadFile("./src/index.js");
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
---
+++ src/todoList/TodoList.js
@@ -0,0 +1,12 @@
+import "./TodoList.css";
+const TodoList = () => {
+  return (
+    <ul>
+      <li>Learn React</li>
+      <li>Bulid tools</li>
+      <li>Record</li>
+    </ul>
+  );
+};
+
+export default TodoList;
